0cbfd1c79550b8df43099e4e9bc2f8a5062ad131
RCP-2001 <coalgolem.xd@gmail.com>
2023-01-12 09:50:51 -0800

    Some Tree stuff
    Will for sure need some more changes

---
+++ VisioAddIn1/VisioAddIn1/Tree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class TreeNode
+{
+	// Values stored in Hostname CSV
+	// Idk if this is the best way to store these values
+	private string Alias;
+	private string Networks;
+	private string Type; // Would Enum work better?
+	private string IPAddress; // Not sure this should be a string?
+	private string subtnet;
+	private string MACAddr;
+	private string gateway;
+	private string controller;
+	private string[] ports; // am i doing c# arrays right?
+	private string mode;
+	private string ColoringVLAN;
+	private string SNMP;
+	private string PTP;
+	private string CollapseARPRequest; // should be a bool?
+	private string NTPServers
+
+	// Private Functions for drawing
+
+
+
+	
+
+
+	public TreeNode()
+	{ 
+	}
+}
